eec9464d2e2425486cafe0cefb4856d4ce2062d1
Erik Slowikowski <e.slowikowski@bigpoint.net>
2017-01-13 16:10:06 +0100

    slider function added

---
+++ Assets/audioscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioscript : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource ButtonHighlightSound;
+
+    [SerializeField]
+    private Slider SFXSlider;
+
+    [SerializeField]
+    private Slider MasterSlider;
+
+    public void Start()
+    {
+        SFXSlider.value = ButtonHighlightSound.volume;
+        MasterSlider.value = AudioListener.volume;
+    }
+
+	public void SFXController()
+    {
+        ButtonHighlightSound.volume = SFXSlider.value;
+    }
+
+    public void MasterVolume()
+    {
+        AudioListener.volume = MasterSlider.value;
+    }
+}
